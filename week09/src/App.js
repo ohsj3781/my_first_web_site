73cbfde2507831cae42792f8c4b92290793e80ce
ohsj3781 <ohsj3781@gmail.com>
2024-11-02 19:58:51 +0900

    init assignment02 branch

--- week09/src/App.js
+++ week09/src/App.js
@@ -1,49 +1,26 @@
-import {createContext,useContext, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import React from "react"
 
-const NameContext=createContext();
+const Couter=()=>{
+  const [count,setCount]=React.useState(0);
 
-const NameProvider
+  const onIncrease=()=>{
+    setCount((prevCount)=>prevCount+1);
+  };
 
-const Home = ({ name }) => {
-  return (
-    <div className="text-4xl font-extrabold">
-      <h1>It's Name {name}</h1>
+  return(
+    <div>
+      <h1>{count}</h1>
+      <div onClick={onIncrease}>Click Me </div>
     </div>
   );
 };
 
-const About = ({ name }) => {
-  return (
-    <div className="text-4xl text-red-400 font-extrabold">
-      <h1>It's About {name}</h1>
-    </div>
+const App=()=>{
+  return(
+    <>
+      <Couter/>
+    </>
   );
 };
-
-function App() {
-  const [name, setName] = useState("Nala Sinephro");
-  return (
-    <Router>
-      <nav className="">
-        <ul
-          className={
-            'flex justify-start gap-10 text-slate-500;'}
-        >
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home name={name} />} />
-        <Route path="/about" element={<About name={name} />} />
-      </Routes>
-    </Router>
-  );
-}
 
 export default App;
